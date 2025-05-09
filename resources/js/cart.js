554ec2320aca1d82839d1c4653e3e41b8f04203e
Vũ Quang Sơn <sonvq@overate-vntech.com>
2025-05-09 17:38:29 +0700

    dasdad

--- resources/js/cart.js
+++ resources/js/cart.js
@@ -148,6 +148,7 @@ const Cart = {
         // Update cart page if we're on it
         if ($('#cartPage').length) {
             this.updateCartPage();
+            this.updateCartSummary();
         }
         
         // Update checkout button state
